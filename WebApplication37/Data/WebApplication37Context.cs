66909f69c71a2b658ac85054ec752d3929c8fd12
home <home@WINDOWS-PQ21EPG>
2022-01-17 11:22:06 +0530

    Add project files.

---
+++ WebApplication37/Data/WebApplication37Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication37.Models;
+
+namespace WebApplication37.Data
+{
+    public class WebApplication37Context : DbContext
+    {
+        public WebApplication37Context (DbContextOptions<WebApplication37Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication37.Models.Customer> Customer { get; set; }
+    }
+}
